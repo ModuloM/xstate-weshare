24428c81ddf1a14277d21d37e3c9c9c7915cd163
Antoine Le Taxin <aletaxin@antoines-mbp.home>
2024-03-12 15:32:50 +0100

    chore: initial commit 🚀

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['setupTests.ts'],
+    include: ['src/**/*.test.{ts,tsx}'],
+    clearMocks: true,
+  },
+})
