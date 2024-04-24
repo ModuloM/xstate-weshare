204161bdd94723ba5c00638169faf3da078b4462
Antoine Le Taxin <antoine.letaxin@wescale.fr>
2024-04-24 16:47:44 +0200

    feat: implement authent 1 example

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,45 @@
 import './App.css'
 
 import {useMachine} from "@xstate/react";
+import {useEffect} from "react";
 
 import {authenticationMachine} from "./state/authentication/authentication.machine.ts";
-import {useEffect} from "react";
 
 function App() {
 
-  const [state, send] = useMachine(authenticationMachine)
+  const [snapshot, send] = useMachine(authenticationMachine)
 
   const handleLogin = () => {
     send({ type: 'login' })
   }
 
   useEffect(() => {
-    console.log({ context: state.context })
-  }, [state])
+    console.log({ context: snapshot.context })
+  }, [snapshot])
 
   return (
     <main>
-      {state.context.status === 'authenticated'
+      {snapshot.context.status === 'authenticated'
         ? (
           <div className="card">
             <div className="authenticated">
-              I’m authenticated 🔓
+              You are authenticated 🔓
             </div>
           </div>
         ) : (
           <div className="card">
             <div className="unauthenticated">
-              I’m not authenticated 🔒
+              Not authenticated 🔒
             </div>
-            <button onClick={handleLogin}>🔐 Authenticate</button>
+            {snapshot.context.isLoading ? (
+                <div className="message">
+                  <div className="loader"></div>
+                  &nbsp;&nbsp; Loading
+                </div>
+              ) : (
+                <button onClick={handleLogin}>🔐 Authenticate</button>
+              )
+            }
           </div>
         )
       }
--- src/state/authentication/authentication.machine.ts
+++ src/state/authentication/authentication.machine.ts
@@ -1,37 +1,121 @@
-import {assign, setup} from 'xstate';
+import { assign, fromPromise, setup } from 'xstate'
+
+import { loginQuery } from './authentication.queries.ts'
+import type { User } from './types.ts'
+
+const AuthenticationStatus = {
+  authenticated: 'authenticated',
+  unauthenticated: 'unauthenticated',
+} as const
 
 type AuthenticationContextType = {
-  status: 'authenticated' | 'unauthenticated'
+  status: keyof typeof AuthenticationStatus
+  isLoading: boolean
+  user: User | null
 }
 
 type AuthenticationEventType =
   | { type: 'login' }
+  | { type: 'logout' }
+  | { type: 'setIsLoading' }
+  | { type: 'setAuthentication', output: User | Error }
+  | { type: 'handleIsAuthenticated' }
+  | { type: 'setError' }
+  | { type: 'handleError' }
 
 export const authenticationMachine = setup({
   types: {
-    // Obligation to cast here
     context: {} as AuthenticationContextType,
     events: {} as AuthenticationEventType,
   },
   actions: {
-    login: () => {
-      console.log('login action called')
+    setIsLoading: assign({
+      isLoading: true,
+    }),
+    setAuthentication: assign({
+      user: ({ event }) => (event as Extract<AuthenticationEventType, { type: 'setAuthentication' }>).output,
+      isLoading: false,
+      status: 'authenticated',
+    }),
+    handleIsAuthenticated: () => {
+      console.log('save authentication somewhere')
+    },
+    setError: () => {
+      // Add your action code here
+      // ...
+    },
+    handleError: () => {
+      // Add your action code here
+      // ...
     },
   },
+  actors: {
+    authenticate: fromPromise<User, { userName: string, password: string }>(async ({ input }) => await loginQuery(input)),
+  },
 }).createMachine({
-  initial: 'idle',
   context: {
     status: 'unauthenticated',
+    isLoading: false,
+    user: null,
   },
-  states : {
-    idle: {
+  id: 'Authentication - 1 - Login',
+  initial: 'Unauthenticated',
+  states: {
+    Unauthenticated: {
       on: {
         login: {
-          actions: assign({
-            status: 'authenticated',
-          }),
+          target: 'ProcessAuthentication',
         },
-      }
-    }
-  }
+      },
+    },
+    ProcessAuthentication: {
+      entry: {
+        type: 'setIsLoading',
+      },
+      invoke: {
+        id: 'login',
+        src: 'authenticate',
+        input: {
+          userName: 'me',
+          password: 'psw',
+        },
+        onDone: {
+          target: 'Authenticated',
+          // actions: [
+          //   assign({
+          //     user: ({ event }) => event.output, // here event is strongly typed
+          //     isLoading: false,
+          //     status: 'authenticated',
+          //   }),
+          // ]
+          actions: [
+            {
+              type: 'setAuthentication', // here we have to cast event type
+            },
+            {
+              type: 'handleIsAuthenticated',
+            },
+          ],
+        },
+        onError: {
+          target: 'Unauthenticated',
+          actions: [
+            {
+              type: 'setError',
+            },
+            {
+              type: 'handleError',
+            },
+          ],
+        },
+      },
+    },
+    Authenticated: {
+      on: {
+        logout: {
+          target: 'Unauthenticated',
+        },
+      },
+    },
+  },
 })
