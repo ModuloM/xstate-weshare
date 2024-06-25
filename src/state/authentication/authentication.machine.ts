4fc555c3b1a89c3e38a57a351e8b2805bee118a7
Antoine Le Taxin <antoine.letaxin@wescale.fr>
2024-06-25 15:14:29 +0200

    fix: add init state

--- src/state/authentication/authentication.machine.ts
+++ src/state/authentication/authentication.machine.ts
@@ -81,15 +81,16 @@ export const authenticationMachine = setup({
     authenticate: fromPromise<User, { userName: string, password: string }>(async ({ input }) => await loginQuery(input)),
   },
 }).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QEECuAXAFmAduglgMYCGBA9jgAQC0lAjDZQDJlT44B0AkjvugMQBtAAwBdRKAAOZWH3wUJIAB6IAzHTocAbACYArMLrDVAdj07VegJwAaEAE9EOrcI5WddABxat5iwBZnAF8guzQsXAIScipaBloWNk4ePiE6cSQQaVkYxRUEdU1dAyNTc0tbB0RPTX8reqsTT08mnWFhKxCwjGw8IlJ5WPpGRPYOAFUcYh7I-vRIfgAbVnYRDKkZOQVM-LodZw5VK2E9LTo6nxMrTztHBAMtDk9VF-P-PX9jOlUukHDeqIDCiMeLMFacAAKACcyIQ4LB-rNooN+BAKGAOOwAG5kADWGOWSTWimyWxweTUGm0+kMxjMFmstzUwke7i8WlMnkanhOv0RfWRwLiI3BHGhsPh-MBMX4YChMKhHEki1IADMyFCALYcQmrMQkza5HaUoo00r0ipMhC6EwcPTPF4eFnOUx8mYCoFDUGjThSuYLQlkDDEzKko2gfKeHT+Dg04xWc5aExJnRWyyaYQO-znEw6erCExuiIemIgkVJDh+6ILJSwdCkDHEVXzKEACiM7QAlPwq56ywlRb35hAQxscoMKQgozG40dE8nc1bo7a6g0rPazM0dEWAXNBv2wRXxXDYLAAEq4MAAdwAKnjcEJ9aHDRPjVODNoOgYdFG2nRF1UCB0PUbhrr4Hh6EcO5In2woDkeMInrAkxDiio5ZC+2wRogewHEcJxnBcSbXGm5h2g6qhaOuHanFoIShCAOBkBAcCKKhQrDPB7AGuOWHKIg1BaFagkcO0YnieJ5zQSW+5wYeYwpOgPFkpOgRLh4HCBOydQ1Ho5gctJ0qyZx8mcJM0zFtKkDKeG-FAS8MaeNY+iBM8Gj+Fa36aRRpxWKohhnHRDHsV65ZjMekrukZfFhq+2EIO8HBeGYljmB0Ji5loHmAXoGWae5kH+KYgRNIZe4cd6g5Rf6EA2XFdkfDGqhFRl6gJv4HXZXcP56G49qOr5TS5WVgqhVxkKIfCF44Ned74uSz68Qt8VWIlJhfOu-i6KoHgmEuHTka8lGqNOHWeCNsEmT6YqTaeKHVaNdV8fkq29etbUfNtu1pnQvWZg5-jeP+VhUXo9FBEAA */
   context: {
     status: 'unauthenticated',
     isLoading: false,
     user: null,
   },
   id: 'Authentication - 1 - Login',
-  initial: 'Unauthenticated',
+  initial: 'Init',
   states: {
-    Unauthenticated: {
+    Init: {
       entry: {
         type: 'getAuthenticationInfo',
       },
@@ -104,6 +105,8 @@ export const authenticationMachine = setup({
           target: "Unauthenticated",
         },
       ],
+    },
+    Unauthenticated: {
       on: {
         login: {
           target: 'ProcessAuthentication',
