89eed6b8c3897903d5ddbe28a9bd3c7109c0507f
Antoine Le Taxin <antoine.letaxin@wescale.fr>
2024-06-25 14:52:20 +0200

    fix: add init state

--- src/state/authentication/authentication.machine.ts
+++ src/state/authentication/authentication.machine.ts
@@ -80,15 +80,16 @@ export const authenticationMachine = setup({
     authenticate: fromPromise<User, { userName: string, password: string }>(async ({ input }) => await loginQuery(input)),
   },
 }).createMachine({
+  /** @xstate-layout N4IgpgJg5mDOIC5QEECuAXAFmAduglgMYCGBA9jgAQC0lAjDZQDJlT44B0AkjvugMQBtAAwBdRKAAOZWH3wUJIAB6IAzMLocAbACYALAE4AHBtVGtdPUYA0IAJ6I6qgKwcDz1XX0G9e3c6cAX0DbNCxcAhJyKloGWhY2Th4+ITpxJBBpWWjFFQR1TV1DEydzSxt7RD11DgB2YU9VXy0tPR0jVWDQjGw8IlJ5GPpGBPYOAFUcYh6I-vRIfgAbVnYRdKkZOQUMvMt6jm9a2tUdOmEjP1sHfOcDDhOdAzozveEdWucukDDeyIGKRhxZgrTgABQATmRCHBYD9ZlFBvwIBQwBx2AA3MgAa1Ry0Sa0UWS2OFyjj0+0Ox1O50ulQQRk0zmEzK8rQMOh0JzoXzhfQRANiIxBHAhUJhvL+0X4YHBkPBHEki1IADMyOCALYcPGrMSEzY5HaODyqDh+C7GApadmqK6ITl3J4tIwcuhsvTCT4hb4zPn-IZA0acCVzBZ4sgYAkZIkG0B5Ex6A6GAxaJrOZwdFy2hDvHR1BpOYRaYS1dxGAw8n2SwaAoWJEWQ6GwWCTYP8nBCXVR-WDUkIZ4+A7J46GW4Mi5Z94JnT59peOjOIu+YJenBkCBwRStv01+IgvXZHuGhDULRZk8cZmXq-X2oV8K+6I74F15LoffE3ttLNeBPuTycq0-GOIw71+OZq0FXc60maZ70lSB3xjZRHHaO5VAMYRqkLcxCz0CcTFNDwvFqHQU3UEtQPhbdIOfMZRUbWFK3A7YNgPFjkL7LQGXuLxpzoDCnHQid3g4cxjEwk4Uy0Ejby9LdHxowMOHkhCuzYkkjw8BMmlUD5mRLV19CzAxajzRpfDLAJjk9bo4OY-1azohsYRbJi20Qw9Y0QJ4TSLdlk10kdqgnVQtDM4jMI6d4tGXQIgA */
   context: {
     status: 'unauthenticated',
     isLoading: false,
     user: null,
   },
   id: 'Authentication - 1 - Login',
-  initial: 'Unauthenticated',
+  initial: 'Init',
   states: {
-    Unauthenticated: {
+    Init: {
       entry: {
         type: 'getAuthenticationInfo',
       },
@@ -103,6 +104,8 @@ export const authenticationMachine = setup({
           target: "Unauthenticated",
         },
       ],
+    },
+    Unauthenticated: {
       on: {
         login: {
           target: 'ProcessAuthentication',
